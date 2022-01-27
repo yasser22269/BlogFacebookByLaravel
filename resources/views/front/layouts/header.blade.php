a46525f299244adcd48e038a9e9453a00f16ae48
Yasser Abd Elghany <yasser.m22291@gmail.com>
2022-01-27 19:53:32 +0200

    photos

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="" />
     <meta name="keywords" content="" />
 	<title>Winku Social Network Toolkit</title>
-    <link rel="icon" href="images/fav.png" type="image/png" sizes="16x16">
+    <link rel="icon" href="{{asset('/front/')}}/images/fav.png" type="image/png" sizes="16x16">
 
     <link rel="stylesheet" href="{{asset('/front/')}}/css/main.min.css">
     <link rel="stylesheet" href="{{asset('/front/')}}/css/style.css">
